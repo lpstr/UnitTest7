75e94826ea12fc56dec691991f696df372a373ff
bbekyarov@gmail.com <bbeky@softserveinc.com>
2023-06-27 12:55:03 +0300

    UC#7 Create unit tests

---
+++ UnitTest7/UnitTest7/StudentConverter.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest7
+{
+
+    public class StudentConverter
+
+    {
+
+        public List<Student> ConvertStudents(List<Student> students)
+
+        {
+
+            return students.Select(student =>
+
+            {
+                if (students == null)
+                {
+                    throw new ArgumentNullException(nameof(students));
+                }
+
+                var result = new Student
+
+                {
+
+                    Name = student.Name,
+
+                    Age = student.Age,
+
+                    Grade = student.Grade
+
+                };
+
+
+
+                if (student.Grade > 90)
+
+                {
+
+                    if (student.Age < 21)
+
+                    {
+
+                        result.Exceptional = true;
+
+                    }
+
+                    else
+
+                    {
+
+                        result.HonorRoll = true;
+
+                    }
+
+                }
+
+                else if (student.Grade > 70)
+
+                {
+
+                    result.Passed = true;
+
+                }
+
+                else
+
+                {
+
+                    result.Passed = false;
+
+                }
+
+
+
+                return result;
+
+            }).ToList();
+
+        }
+
+    }
+}
--- UnitTest7/UnitTestProject/UnitTest1.cs
+++ UnitTest7/UnitTestProject/UnitTest1.cs
@@ -1,11 +1,69 @@
 namespace UnitTestProject
 {
-    public class UnitTest1
+    public class StudentConverterTests
     {
         [Fact]
-        public void Test1()
+        public void Should_Set_HonorRoll_When_Student_Grade_Above_90_And_Age_Above_21()
         {
+            var students = new List<Student> { new Student { Name = "John", Age = 21, Grade = 95 } };
+            var converter = new StudentConverter();
 
+            var results = converter.ConvertStudents(students);
+
+            Assert.True(results[0].HonorRoll);
+        }
+
+        [Fact]
+        public void Should_Set_Exceptional_When_Student_Grade_Above_90_And_Age_Below_21()
+        {
+            var students = new List<Student> { new Student { Name = "John", Age = 20, Grade = 95 } };
+            var converter = new StudentConverter();
+
+            var results = converter.ConvertStudents(students);
+
+            Assert.True(results[0].Exceptional);
+        }
+
+        [Fact]
+        public void Should_Set_Passed_When_Student_Grade_Between_71_And_90()
+        {
+            var students = new List<Student> { new Student { Name = "John", Age = 20, Grade = 80 } };
+            var converter = new StudentConverter();
+
+            var results = converter.ConvertStudents(students);
+
+            Assert.True(results[0].Passed);
+        }
+
+        [Fact]
+        public void Should_Set_Passed_False_When_Student_Grade_Below_71()
+        {
+            var students = new List<Student> { new Student { Name = "John", Age = 20, Grade = 70 } };
+            var converter = new StudentConverter();
+
+            var results = converter.ConvertStudents(students);
+
+            Assert.False(results[0].Passed);
+        }
+
+        [Fact]
+        public void Should_Return_Empty_List_When_Given_Empty_List()
+        {
+            var students = new List<Student>();
+            var converter = new StudentConverter();
+
+            var results = converter.ConvertStudents(students);
+
+            Assert.Empty(results);
+        }
+
+        [Fact]
+        public void Should_Throw_ArgumentNullException_When_Given_Null()
+        {
+            List<Student> students = null;
+            var converter = new StudentConverter();
+
+            Assert.Throws<ArgumentNullException>(() => converter.ConvertStudents(students));
         }
     }
 }
